2fa6492504d2207bd16c871399a4409fc7e34887
Yanus Poluektovich <ypoluektovich@gmail.com>
2013-02-13 18:36:37 +0400

    Improved build scripts

--- retrieve.lib.sources.sh
+++ retrieve.lib.sources.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-mkdir -p lib-src
-curl https://oss.sonatype.org/service/local/repositories/releases/content/org/yaml/snakeyaml/1.11/snakeyaml-1.11-sources.jar -o lib-src/snakeyaml-1.11-sources.jar
-curl https://oss.sonatype.org/service/local/repositories/central/content/org/apache/ivy/ivy/2.2.0/ivy-2.2.0-sources.jar -o lib-src/ivy-2.2.0-sources.jar
+#mkdir -p lib-src
+#curl https://oss.sonatype.org/service/local/repositories/releases/content/org/yaml/snakeyaml/1.11/snakeyaml-1.11-sources.jar -o lib-src/snakeyaml-1.11-sources.jar
+#curl https://oss.sonatype.org/service/local/repositories/central/content/org/apache/ivy/ivy/2.2.0/ivy-2.2.0-sources.jar -o lib-src/ivy-2.2.0-sources.jar
+ivy -settings ivysettings.xml -confs sources -retrieve 'lib-src/[artifact]-[revision](-[classifier]).[ext]'
