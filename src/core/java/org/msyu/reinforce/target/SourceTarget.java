bd761f99730fd33140d6e67959af56c31e6925f5
Yanus Poluektovich <ypoluektovich@gmail.com>
2013-02-18 12:18:07 +0400

    Support all features of resource collection definitions in Source targets

--- src/core/java/org/msyu/reinforce/target/SourceTarget.java
+++ src/core/java/org/msyu/reinforce/target/SourceTarget.java
@@ -3,22 +3,19 @@ package org.msyu.reinforce.target;
 import org.msyu.reinforce.ExecutionException;
 import org.msyu.reinforce.Target;
 import org.msyu.reinforce.TargetInitializationException;
-import org.msyu.reinforce.resources.EagerlyCachingFileTreeResourceCollection;
 import org.msyu.reinforce.resources.Resource;
 import org.msyu.reinforce.resources.ResourceAccessException;
 import org.msyu.reinforce.resources.ResourceCollection;
+import org.msyu.reinforce.resources.ResourceDefinitionYamlParser;
 import org.msyu.reinforce.resources.ResourceEnumerationException;
 import org.msyu.reinforce.resources.ResourceIterator;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 import java.util.Map;
 
 public class SourceTarget extends Target implements ResourceCollection {
-
-	public static final String LOCATION_KEY = "location";
 
 	private ResourceCollection myResourceCollection;
 
@@ -28,14 +25,7 @@ public class SourceTarget extends Target implements ResourceCollection {
 
 	@Override
 	protected void initTarget(Map docMap, Map<String, Target> dependencyTargetByName) throws TargetInitializationException {
-		if (!docMap.containsKey(LOCATION_KEY)) {
-			throw new TargetInitializationException("missing parameter '" + LOCATION_KEY + "' of source target");
-		}
-		Object source = docMap.get(LOCATION_KEY);
-		if (!(source instanceof String)) {
-			throw new TargetInitializationException("parameter '" + LOCATION_KEY + "' of source target must be a string");
-		}
-		myResourceCollection = new EagerlyCachingFileTreeResourceCollection(Paths.get((String) source));
+		myResourceCollection = ResourceDefinitionYamlParser.parseAsCollection(docMap, dependencyTargetByName);
 	}
 
 	@Override
